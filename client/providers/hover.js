818f9044b7ff8049cd83a21e323904d359286a96
dyu <david.yu.ftw@gmail.com>
2024-10-04 01:07:41 +0800

    backport fixes

--- client/providers/hover.js
+++ client/providers/hover.js
@@ -8,7 +8,7 @@ class HTMLHoverProvider {
         this._htmlLanguageService = vscode_html_languageservice_1.getLanguageService();
         this._cssLanguageService = vscode_css_languageservice_1.getCSSLanguageService();
         // private _expression = /(html\s*`)([^`]*)(`)/g
-        this._expression = /(\/\*\s*html\s*\*\/\s*`|html\s*`)([^`]*)(`)/g;
+        this._expression = /(\/\*\s*html\s*\*\/\s*`|(?<!`)html\s*`)([^`]*)(`)/g;
     }
     provideHover(document, position, token) {
         const currentOffset = document.offsetAt(position);
